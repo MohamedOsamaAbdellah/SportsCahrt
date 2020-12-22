b4ff3876d58b6a91c583f46bf395f35b5b073f4b
MohamedOsamaAbdellah <36635247+MohamedOsamaAbdellah@users.noreply.github.com>
2020-12-22 12:08:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Charts , RadarChart } from './Components';
 class App extends React.Component {
     render(){
         return(
-            <div style={{'display':'flex','backgroundColor':'#050612'}}>
+            <div style={{'display':'flex','height':'800px','backgroundColor':'#050612'}}>
                 <Charts />
                 <RadarChart />
             </div>
